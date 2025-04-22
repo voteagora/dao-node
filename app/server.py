ba28596912e49ce857f26bc0374ae96a418c5221
Jeff <jeff@voteagora.com>
2025-04-22 17:53:03 -0400

    Add ENSGovernor

--- app/server.py
+++ app/server.py
@@ -791,7 +791,7 @@ async def bootstrap_event_feeds(app, loop):
     proposals = Proposals(governor_spec=public_config['governor_spec'], modules=modules)
 
     gov_spec_name = public_config['governor_spec']['name']
-    if gov_spec_name == 'compound':
+    if gov_spec_name in ('compound', 'ENSGovernor'):
         PROPOSAL_CREATED_EVENTS = [PROPOSAL_CREATED_1]
     elif gov_spec_name == 'agora' and public_config['governor_spec']['version'] == 0.1:
         PROPOSAL_CREATED_EVENTS = [PROPOSAL_CREATED_1, PROPOSAL_CREATED_2, PROPOSAL_CREATED_3, PROPOSAL_CREATED_4]
@@ -805,7 +805,7 @@ async def bootstrap_event_feeds(app, loop):
         app.ctx.register(f'{chain_id}.{gov_addr}.' + PROPOSAL_EVENT, proposals)
 
     VOTE_EVENTS = [VOTE_CAST_1]    
-    if public_config['governor_spec']['name'] != 'compound':
+    if not (public_config['governor_spec']['name'] in ('compound', 'ENSGovernor')):
         VOTE_EVENTS.append(VOTE_CAST_WITH_PARAMS_1)
 
     votes = Votes(governor_spec=public_config['governor_spec'])
