cde8d7a1bbdf18c53cead8c2da6972549e1dee75
Pedro Fernandes <pedrohasftb@gmail.com>
2025-04-23 15:39:43 -0300

    .get() pattern and fix test

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -77,11 +77,11 @@ async def test_proposals_types_endpoint(app, test_client, pguild_ptc_abi):
     assert resp.status == 200
     assert len(resp.json) == 1
 
-    expected_array_element = {'quorum': 0, 'approval_threshold': 0, 'name': 'Signal Votes', 'scopes': []}   
+    expected_array_element = {'quorum': 0, 'approval_threshold': 0, 'name': 'Signal Votes', 'module': '0x4414d030cffec5edc011a27c653ce21704b12d85', 'scopes': []}   
     proposal_type_id = '0' 
     assert expected_array_element == resp.json['proposal_types'][proposal_type_id]
 
-    expected_array_element = {'quorum': 0, 'approval_threshold': 5100, 'name': 'Distribute Splits', 'scopes': [{'scope_key': '02b27a65975a62cd8de7d22620bc9cd98e79f9042d3f5537', 'block_number': 8118843, 'transaction_index': 66, 'log_index': 113, 'selector': '2d3f5537', 'description': 'Distribute splits contract', 'disabled_event': {}, 'deleted_event': {}, 'status': 'created'}]}
+    expected_array_element = {'quorum': 0, 'approval_threshold': 5100, 'name': 'Distribute Splits', 'module': '0x0000000000000000000000000000000000000000', 'scopes': [{'scope_key': '02b27a65975a62cd8de7d22620bc9cd98e79f9042d3f5537', 'block_number': 8118843, 'transaction_index': 66, 'log_index': 113, 'selector': '2d3f5537', 'description': 'Distribute splits contract', 'disabled_event': {}, 'deleted_event': {}, 'status': 'created'}]}
     proposal_type_id = '1'
     assert expected_array_element == resp.json['proposal_types'][proposal_type_id]
 
