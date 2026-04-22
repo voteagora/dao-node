1989184e32dc36ddc86a1fd03e9a7cc9e6eed92e
jefag <jeff@voteagora.com>
2026-04-21 20:55:21 -0400

    Add approx block counts

--- app/server.py
+++ app/server.py
@@ -1319,6 +1319,7 @@ async def integrity(request):
     out['delegations.delegatee_vp_history'] = check(app.ctx.delegations.delegatee_vp_history)
     out['delegations.delegatee_cnt'] = check(app.ctx.delegations.delegatee_cnt)
     out['delegations.delegatee_vp'] = check(app.ctx.delegations.delegatee_vp)
+    out['delegations.approx_block_counts'] = app.ctx.delegations.approx_block_counts
     
     out['pass'] = all([v['pass'] for v in out.values()])
     
