cb299a07a917dce80b12699d5bf96695cf1120b6
Pedro Fernandes <pedrohasftb@gmail.com>
2026-05-01 15:03:33 -0300

    fix 'list' object has no attribute 'items'

--- app/server.py
+++ app/server.py
@@ -1130,7 +1130,7 @@ async def delegate_handler(app, request, addr):
 
     addr = addr.lower()
 
-    delegatee_list = app.ctx.delegations.delegatee_list.get(addr, [])
+    delegatee_list = app.ctx.delegations.delegatee_list.get(addr, {})
     delegation_amounts = app.ctx.delegations.delegation_amounts.get(addr, {})
 
 
